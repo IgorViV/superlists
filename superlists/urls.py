85047d463211377508e78447aa381d79bcc7be2f
Igor Vlashchenko <iupgrader@mail.ru>
2025-05-18 14:12:46 +0300

    Базовое представление теперь возвращает минимальный HTML

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', views.home_page, name='home')
 ]
